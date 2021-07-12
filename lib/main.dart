506019d8b6d84fd55a5b7f789f281768443112d0
Metgraph <loydmajk61@gmail.com>
2021-07-12 22:48:50 +0200

    remove useless comments

--- lib/main.dart
+++ lib/main.dart
@@ -6,20 +6,12 @@ void main() {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Profili'),
@@ -84,8 +76,6 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
       body: Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
           child: ListView(
         children: boxes,
       )), // This trailing comma makes auto-formatting nicer for build methods.
